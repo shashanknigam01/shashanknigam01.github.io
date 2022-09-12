76bf0375878f01bedf9cf3e3ef39c9ddd292bbd5
shashanknigam01 <43205696+shashanknigam01@users.noreply.github.com>
2022-09-12 14:42:20 +0530

    Update pushlogic.js

--- webpush/pushlogic.js
+++ webpush/pushlogic.js
@@ -1,6 +1,4 @@
 // Import the functions you need from the SDKs you need
-  importScripts("https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js");
-  importScripts("https://www.gstatic.com/firebasejs/9.9.3/firebase-messaging.js");
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
