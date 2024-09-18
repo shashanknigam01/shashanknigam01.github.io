b1f72fcd626cacb9d39515616f260ccc417c83cd
shashanknigam01 <43205696+shashanknigam01@users.noreply.github.com>
2024-09-18 20:01:23 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -18,6 +18,6 @@ const isSupported = firebase.messaging.isSupported();
 if (isSupported) {
     const messaging = firebase.messaging();
     messaging.onBackgroundMessage(({ notification: { title, body, image } }) => {
-        self.registration.showNotification(title, { body, icon: image || '/assets/icons/icon-72x72.png' });
+        self.registration.showNotification(title, { body, icon: image || 'https://shashanknigam01.github.io/favicon.ico' });
     });
 }
