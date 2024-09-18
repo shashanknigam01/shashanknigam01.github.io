4373514ec4054b8005f8aea9802237c8a08d40c3
shashanknigam01 <43205696+shashanknigam01@users.noreply.github.com>
2024-09-18 17:21:59 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,3 @@
-importScripts("https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/10.13.1/firebase-messaging.js");
-
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
