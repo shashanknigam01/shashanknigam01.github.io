e681105f77f277cf87f63a79afa32b4835f51988
shashanknigam01 <43205696+shashanknigam01@users.noreply.github.com>
2024-09-19 18:06:17 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,2 @@
 importScripts('https://www.gstatic.com/firebasejs/10.13.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.13.1/firebase-messaging-compat.js');
-
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
-firebase.initializeApp({
-  apiKey: "AIzaSyAqjqQRL7Tvsi4HzwJw-CHh6avJg6iPdUY",
-  authDomain: "shashankproject2.firebaseapp.com",
-  projectId: "shashankproject2",
-  storageBucket: "shashankproject2.appspot.com",
-  messagingSenderId: "729315975437",
-  appId: "1:729315975437:web:d7d3c0acb10919f999d044"
-});
